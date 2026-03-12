688c386ba2a43e597ae3f04f2c548ab3845740c2
Aryan Pareek <arryan0112@gmail.com>
2026-03-12 19:00:56 +0530

    complete_transformer_block

--- src/model.py
+++ src/model.py
@@ -101,3 +101,23 @@ class FeedForward(nn.Module):
 
     def forward(self, x):
         return self.net(x)    
+    
+
+class TransformerBlock(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+
+        self.ln1 = nn.LayerNorm(config.emb_dim)
+        self.attn = CausalSelfAttention(config)
+
+        self.ln2 = nn.LayerNorm(config.emb_dim)
+        self.ff = FeedForward(config)
+
+    def forward(self, x):
+
+        x = x + self.attn(self.ln1(x))
+
+        x = x + self.ff(self.ln2(x))
+
+        return x    
